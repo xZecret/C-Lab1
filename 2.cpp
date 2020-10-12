234dfc6fea0538a1f5991562a69eed165d4d9528
Адам Тхаркахов <70707610+xZecret@users.noreply.github.com>
2020-10-12 23:41:40 +0300

    Create 2.cpp

---
+++ 2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+ {
+	//Lab 2.2
+	cout << "\n Lab 2.2 \n";
+	cout << 1.71 << endl; // double
+	cout << 5.E+2 << endl; // double
+	// cout << 0X1E6 << endl; // integer
+	cout << 1e-2f << endl; // float
+	cout << 1E-6 << endl; // double
+	cout << 0e0 << endl;  // double
+	// cout << 0F << endl; // Error unknown literal
+	cout << -12.3E-6 << endl; // double
+	cout << 0.314159E1F << endl; // float
+	// cout << 0x1A1.5 << endl; // Error const with floating point
+	cout << 1234.56789L << endl; // long double
+	cout << +10e6 << endl; // double
+	cout << .005 << endl; //double
+	cout << 05.5 << endl; // double
+	// cout << 1.0E-10D << endl; // Error literal not found
+	// cout << 123456L << endl; // long
+	cout << 0051E-04 << endl; // double
+	// cout << 0 << endl; // int
+	// cout << 3.1415U << endl; // Error
+	// cout << E - 6 << endl; // Error
+}
