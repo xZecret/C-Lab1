36dd1d6378914855886f83034fb6468705df67b4
Адам Тхаркахов <70707610+xZecret@users.noreply.github.com>
2020-10-12 23:45:29 +0300

    Create 6.cpp

---
+++ 6.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+ {
+	cout << "\nLab2.6\n";
+	int a, b;
+	a = 5;
+	b = 6;
+	a += b;
+	cout << a << endl;
+	a = 5;
+	a = a + b;
+	cout << a << endl;
+	cout << "equivalently" << endl;
+	
+	a = 5;
+	a += b;
+	cout << a << endl;
+	a = 5;
+	a = a + (b);
+	cout << a << endl;
+	cout << "equivalently" << endl;
+}
