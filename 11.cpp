c5bf453918781864cf0c6f15a5ea9cdda57d4e02
Адам Тхаркахов <70707610+xZecret@users.noreply.github.com>
2020-10-12 23:50:00 +0300

    Update 11.cpp

--- 11.cpp
+++ 11.cpp
@@ -2,3 +2,17 @@
 #include <cmath>
 
 using namespace std;
+
+ {
+	cout << "\nLab2.11\n";
+	int a, b, c, d;
+	a = 10;
+	b = 11;
+	c = 12;
+	d = 13;
+	cout << bool(!(a > b) == (a < b)) << endl;
+	cout << bool(!(a < 2 || a>5) == (a > 2 && a < 5)) << endl;
+	cout << bool((!(2 * a == b + 4)) == (2 * a != b + 4)) << endl;
+	cout << bool((!(a < 1 || b < 2 && c < 3)) == (a > 1 && b > 2 || c > 3)) << endl;
+	cout << bool((!(a < b&& c < d)) == (a > b || c > d)) << endl;
+}
