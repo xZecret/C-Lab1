2f3942aaaf401fa3d618052e349eb5a3b462f815
Адам Тхаркахов <70707610+xZecret@users.noreply.github.com>
2020-10-13 00:00:47 +0300

    Create 29.cpp

---
+++ 29.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+ {
+	cout << "\nLab2.29\n";
+	unsigned int n = 0xFFFFFFFF;
+	bool trans = n & 0x80000000;
+	n << 1;
+	if (trans)
+		n |= 1;
+	cout << n << endl;
+}
