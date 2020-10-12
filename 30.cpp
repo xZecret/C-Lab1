79acda87f6255d3054ec077ddad90adc2100b961
Адам Тхаркахов <70707610+xZecret@users.noreply.github.com>
2020-10-13 00:01:14 +0300

    Create 30.cpp

---
+++ 30.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+ {
+	cout << "\nLab2.30\n";
+	cout << "a) " << endl;
+	cout << "short = " << sizeof(short) << " bytes" << endl;
+	cout << "int = " << sizeof(int) << " bytes" << endl;
+	cout << "long = " << sizeof(long) << " bytes" << endl;
+	cout << "float = " << sizeof(float) << " bytes" << endl;
+	cout << "double = " << sizeof(double) << " bytes" << endl;
+	cout << "long double = " << sizeof(long double) << " bytes" << endl;
+	cout << "b) " << endl;
+}
