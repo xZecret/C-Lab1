c5f3035da20dd5c50803ba2964d1594f1063fbf4
Адам Тхаркахов <70707610+xZecret@users.noreply.github.com>
2020-10-12 23:53:39 +0300

    Create 18.cpp

---
+++ 18.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+ {
+	cout << "\nLab2.18\n";
+	int x, k;
+	k = 0;
+	cin >> x;
+	while (x > 0) {
+		k += x % 10;
+		x /= 10;
+	}
+	cout << k << endl;
+}
