c44abca2de13bea383ba780f2448bb279454f556
Адам Тхаркахов <70707610+xZecret@users.noreply.github.com>
2020-10-12 23:51:44 +0300

    Create 14.cpp

---
+++ 14.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+ {
+	cout << "\nLab2.14\n";
+	double d; float f; long lng; int i; short s;
+	s = i = lng = f = d = 100 / 3;
+	printf("s = %hd i = %d lng = %ld f = %f d = %f\n", s, i, lng, f, d);
+	d = f = lng = i = s = 100 / 3;
+	printf("s = %hd i = %d lng = %ld f = %f d = %f\n", s, i, lng, f, d);
+	s = i = lng = f = d = 1000000 / 3;
+	printf("s = %hd i = %d lng = %ld f = %f d = %f\n", s, i, lng, f, d);
+	d = f = lng = i = s = 1000000 / 3;
+	printf("s = %hd i = %d lng = %ld f = %f d = %f\n", s, i, lng, f, d);
+	lng = s = f = i = d = 100 / 3;
+	printf("s = %hd i = %d lng = %ld f = %f d = %f\n", s, i, lng, f, d);
+	f = s = d = lng = i = (double)100 / 3;
+	printf("s = %hd i = %d lng = %ld f = %f d = %f\n", s, i, lng, f, d);
+	s = i = lng = f = d = 100 / (double)3;
+	printf("s = %hd i = %d lng = %ld f = %f d = %f\n", s, i, lng, f, d);
+	f = s = d = lng = i = (double)100 / 3;
+	printf("s = %hd i = %d lng = %ld f = %f d = %f\n", s, i, lng, f, d);
+	i = s = lng = d = f = (double)(100 / 3);
+	printf("s = %hd i = %d lng = %ld f = %f d = %f\n", s, i, lng, f, d);
+}
