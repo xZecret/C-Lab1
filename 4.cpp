e873524cdf681ac43b395886855e01825f4a5191
Адам Тхаркахов <70707610+xZecret@users.noreply.github.com>
2020-10-12 23:43:01 +0300

    Create 4.cpp

---
+++ 4.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+ {
+	cout << "\n Lab 2.4 \n";
+	int a, c;
+	c = 5;
+
+	c++;
+	a = 2 * c; // правильно
+	cout << a << endl;
+
+	++c;
+	a = c + c; // правильно
+	cout << a << endl;
+
+	//a = 2 * c++; 
+	//cout << a << endl;
+
+	a = ++c + c; // правильно
+	cout << a << endl;
+
+	c += 1;
+	a = c + c; // правильно 
+	cout << a << endl;
+
+	//a = c += 1 + c;
+	//cout << a << endl;
+
+	//a = c++ + c;
+	//cout << a << endl;
+
+	a = (c += 1) + c;
+	cout << a << endl;
+}
