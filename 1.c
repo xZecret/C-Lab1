e787988937034358467d8f23e6025c9b5d221ebc
Адам Тхаркахов <70707610+xZecret@users.noreply.github.com>
2020-10-12 23:39:17 +0300

    Update and rename README.md to 1.c

---
+++ 1.c
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+{
+	cout << "Vipolnil Panin Ilya VPI - 21!" << endl;
+
+	//Lab 2.1
+	cout << "\n Lab 2.1 \n";
+	cout << 123 << endl; // Integer
+	// cout << ‘0’ << endl; // Error
+	cout << 00123 << endl; // Octal number
+	// cout << 'a'U << endl; // Error unknown operator
+	// cout << 1E6 << endl; // Hexadecimal number (type == double)
+	// cout << 058 << endl; // The number goes beyond the category
+	cout << 0xffffffL << endl; // Hexadecimal number
+	cout << 0731UL << endl; //ulong
+	cout << 123456789LU << endl; // unsigned long 
+	// cout << '\x7' << endl; // Error
+	// cout << 01A << endl; // Error unknown literal
+	// cout << ‘\n’ << endl; // Error
+	cout << -5 << endl; // int
+	// cout << 0X - 1AD << endl; // Error unknown literal
+	// cout << -‘x’ << endl; // Error
+	cout << +0xaf << endl; // ASCII
+	cout << 0XFUL << endl; // unsigned long Hexadecimal number 
+	// cout << ‘\122’ << endl; // Error
+	//cout << "x" << endl; // string
+	cout << 0X0 << endl; // unsigned long
+}
