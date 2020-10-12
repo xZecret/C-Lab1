9d056e6524439731c077172fffc6aba9443a4101
Адам Тхаркахов <70707610+xZecret@users.noreply.github.com>
2020-10-12 23:52:39 +0300

    Create 16.cpp

---
+++ 16.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+ {
+	cout << "\nLab2.16\n";
+	int x, x2, x3, x4;
+	cin >> x;
+	x2 = x * x;
+	x3 = x2 * x;
+	x4 = x2 * x2;
+	cout << 2 * x4 - 3 * x3 + 4 * x2 - 5 * x + 6 << endl;
+}
