0d3c4e782dfa6279bfd0762b2f559c0390113eb7
Адам Тхаркахов <70707610+xZecret@users.noreply.github.com>
2020-10-12 23:55:35 +0300

    Create 21.cpp

---
+++ 21.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+ {
+	cout << "\nLab2.21\n";
+	int x;
+	cin >> x;
+	cout << "Proshlo minut: "<< x / 60<< "\nProshlo chasov: "<< x / 3600 << endl;
+}
