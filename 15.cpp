4739aa158cfa64dcaa7f17e02d552cddce3447d9
Адам Тхаркахов <70707610+xZecret@users.noreply.github.com>
2020-10-12 23:52:13 +0300

    Create 15.cpp

---
+++ 15.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+ {
+	cout << "\nLab2.15\n";
+	double d = 3.2, x; int i = 2, y;
+	x = (y = d / i) * 2; printf("x = %f ;y = %d\n", x, y);
+	x = (y = d / i) * 2; printf("x = %d ;y = %f\n", x, y);
+	y = (x = d / i) * 2; printf("x = %f ;y = %d\n", x, y);
+	y = d *(x = 2.5 / d); printf("x = %f; y = %d\n", x, y);
+	x = d *(y = ((int)2.9 + 1.1)) / d; printf("x = %d y = %f\n", x, y);
+
+}
