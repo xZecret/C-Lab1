7ebfdc7768a39dace6612b6c9d7b8b71d53c2921
Адам Тхаркахов <70707610+xZecret@users.noreply.github.com>
2020-10-12 23:47:10 +0300

    Create 8.cpp

---
+++ 8.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+ {
+	cout << "\nLab2.8\n";
+	int i, j, k, m;
+	char c, d;
+	i = 1;
+	j = 2; 
+	k = -7; 
+	m = 0; 
+	c = 'w';
+	
+	d = 'a' + 1 < c;
+	cout << d << endl;
+	
+	//m = 3 < j < 5;
+	//cout << m << endl;
+
+	m = c != 87;
+	cout << m << endl;
+
+	m = !c + 87;
+	cout << m << endl;
+
+	//k = = j - 9 = = i;
+
+	i += ++j + 3;
+	cout << i << endl;
+
+	// 1 + 3 *(n += 7) / 5
+
+	m = -i - 5 * j >= k + 1;
+	cout << m << endl;
+
+	//m = 3 = = j < 5;
+
+	m = c = !87;
+	cout << m << endl;
+
+	//!m = = c + 87;
+
+	k *= 3 + j;
+	cout << k << endl;
+
+	/*k %= m = 1 + n / 2;
+	cout << k << endl;*/
+
+	// cout << c + i < c - 'x' + 10 << endl;
+
+	// i + j++ + k = = -2*j;
+
+	// m = = c = 'w';
+
+	// m = !c = 87;
+
+	/*m != c + 87;
+	cout << m << endl;*/
+
+	// i + j = !k;
+
+	// 1 + 3 * n += 7 / 5;
+
+	//i - k = = '0' + 9 < 10;
+}
