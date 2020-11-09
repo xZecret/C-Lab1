00ee51707873e69d7721d92a04145b39382d35f9
Адам Тхаркахов <70707610+xZecret@users.noreply.github.com>
2020-11-09 23:15:15 +0300

    Update 18.cpp

--- 18.cpp
+++ 18.cpp
@@ -1,17 +1,16 @@
-#include <iostream>
-#include <cmath>
+#include <stdio.h>
 
-using namespace std;
 int main()
-
- {
-	cout << "\nLab2.18\n";
-	int x, k;
-	k = 0;
-	cin >> x;
-	while (x > 0) {
-		k += x % 10;
-		x /= 10;
-	}
-	cout << k << endl;
+{
+    int x, k;
+    printf("x = ");
+    scanf("%d", &x);
+    if ((x > 99) && (x < 1000)){ 
+        k = x % 10; x /= 10;
+        k+= x % 10; x /= 10;
+        k+= x % 10; x /= 10;
+        printf("k = %d", k);
+    }
+    else printf("wrong input");
+    return 0;
 }
